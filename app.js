7b35395f3d809cfa735f57acb640b25eba56d72d
Yolo-cell-hash <keerjar@gmail.com>
2023-11-12 00:41:46 +0530

    Update

--- app.js
+++ app.js
@@ -1,12 +1,9 @@
 const express = require('express');
-const WebSocket = require('ws');
-const ROSLIB = require('roslib');
+const bodyParser = require('body-parser');
 
-const wss = new WebSocket.Server({ port: 9090 });
 const app = express();
-const port = 9090;
+const port = 3000;
 
-const clients = new Map();
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
@@ -17,66 +14,21 @@ app.get('/', function (req, res) {
     res.render('home');
 });
 
+
+
+app.use(bodyParser.json());
+
+// Define a route to handle incoming data
+app.post('/', (req, res) => {
+  const receivedData = req.body.data;
+  console.log('Received data from ROS:', receivedData);
+  // Handle the data as needed
+
+  // Send a response
+  res.status(200).send('Data received successfully');
+});
+
+// Start the backend server
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-});
-
-// Connect to the ROS WebSocket server
-const ros = new ROSLIB.Ros({
-    url: 'ws://localhost:9090'  // Replace with the actual URL of your ROS WebSocket server.
-});
-
-ros.on('connection', () => {
-    console.log('Connected to ROS');
-});
-
-ros.on('error', (error) => {
-    console.error('Error connecting to ROS:', error);
-});
-
-ros.on('close', () => {
-    console.log('Disconnected from ROS');
-});
-
-// Handle WebSocket connections with clients
-wss.on('connection', (ws) => {
-    console.log('A client connected');
-
-    // Store the WebSocket connection in your 'clients' Map
-    clients.set(ws, true);
-
-    ws.on('message', (message) => {
-        // Handle incoming messages from clients
-
-        // You can send data to ROS here using roslibjs
-        // For example:
-        const cmdVel = new ROSLIB.Topic({
-            ros: ros,
-            name: '/cmd_vel',
-            messageType: 'geometry_msgs/Twist',
-        });
-
-        const twist = new ROSLIB.Message({
-            linear: {
-                x: 0.1,
-                y: 0,
-                z: 0,
-            },
-            angular: {
-                x: 0,
-                y: 0,
-                z: 0,
-            },
-            encoding: 'ascii',
-        });
-
-        console.log(`Sending ROS message: ${JSON.stringify(twist)}`);
-        cmdVel.publish(twist);
-    });
-
-    ws.on('close', () => {
-        // Remove the WebSocket connection from the 'clients' Map
-        clients.delete(ws);
-        console.log('A client disconnected');
-    });
+  console.log(`Backend server listening at http://localhost:${port}`);
 });
