128df8d98d0f59adc6cb53e14e6ae70c58b8a5a3
Yolo-cell-hash <keerjar@gmail.com>
2023-12-09 13:57:00 +0530

    Patch

--- app.js
+++ app.js
@@ -1,51 +1,43 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors'); // Added CORS middleware
+const rosnodejs = require('rosnodejs');
+const https = require('https'); // Use the 'https' module for secure connections
 
-const app = express();
+// Initialize ROS node
+rosnodejs.initNode('/ros_listener_node')
+  .then((rosNode) => {
+    // Create a ROS subscriber
+    const sub = rosNode.subscribe('/topic1', 'std_msgs/Float64', (data) => {
+      // Handle incoming data
+      sendDataToBackend(data);
+    });
 
-app.set("view engine", "ejs");
-app.use(express.static("public"));
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+    // Function to send data to the Node.js backend
+    function sendDataToBackend(data) {
+      // Create a JSON payload with the data
+      const payload = JSON.stringify({ data: data });
 
-let dataHistory = [];
+      // Configure the HTTP request options
+      const options = {
+        hostname: 'calm-pink-spider-tux.cyclic.app', // Update this line with your deployed website address
+        port: 443, // Default port for HTTPS
+        path: '/',
+        method: 'POST',
+        protocol: 'https:', // Set the protocol to 'https'
+        headers: {
+          'Content-Type': 'application/json',
+          'Content-Length': payload.length,
+        },
+      };
 
-// CORS configuration
-const corsOptions = {
-  origin: 'http://localhost:11311', // Replace with your ROS bridge domain
-  optionsSuccessStatus: 200,
-};
-app.use(cors(corsOptions));
+      // Send the data to the backend server using 'https'
+      const req = https.request(options, (res) => {
+        console.log(`Backend server responded with status code: ${res.statusCode}`);
+      });
 
-app.use(bodyParser.json());
+      req.on('error', (error) => {
+        console.error(`Error sending data to backend: ${error.message}`);
+      });
 
-app.post('/', (req, res) => {
-  try {
-    const receivedData = req.body.data;
-    console.log('Received data:', receivedData);
-
-    dataHistory.push(receivedData);
-    console.log('Data history:', dataHistory);
-
-    res.redirect('/res');
-  } catch (error) {
-    console.error('Error processing POST request:', error);
-    res.status(500).send('Internal Server Error');
-  }
-});
-
-app.get('/', function (req, res) {
-  res.render('root');
-});
-
-app.get('/res', function (req, res) {
-  res.render('home', { dataHistory: dataHistory });
-});
-
-let port = process.env.PORT || 3000; // Updated port configuration
-
-// Start the backend server
-app.listen(port, () => {
-  console.log(`Backend server listening at http://localhost:${port}`);
-});
+      req.write(payload);
+      req.end();
+    }
+  });
